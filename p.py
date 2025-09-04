689af64d2e36f29d8f0618aecaa852f02c7c1576
TQA6608m_qnb <TQA6608m@qnb.com>
2025-09-04 20:43:58 +0530

    first python script

---
+++ p.py
@@ -0,0 +1,8 @@
+
+user_name = "alstonpaul0406-cmd"
+print('my first script output is below which is the sum of 10 + 25')
+a = 10
+b = 25
+total = a + b
+print('the sum is:', total)
+print('my user name is:', user_name)
